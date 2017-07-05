48170a441bbf29ddd9fdb5e31d841b16a73b8180
Verity <vhonebon@gmail.com>
2017-07-05 14:04:54 +0100

    Added down function and minimum temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,6 +6,12 @@ describe("Thermostat", function() {
     thermostat = new Thermostat();
   });
 
+  it("has a minimum temperature of 10 degrees", function() {
+    expect(thermostat.MINIMUM).toEqual(10);
+  });
+
+
+
 
   it ('initializes with a temperature of 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
@@ -18,4 +24,10 @@ describe("Thermostat", function() {
     });
   });
 
+  describe('down', function() {
+    it ('decreases the temperature', function() {
+      thermostat.down(5)
+      expect(thermostat.temperature).toEqual(15)
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,12 @@
 function Thermostat() {
   this.temperature = 20;
+  this.MINIMUM = 10;
 }
 
 Thermostat.prototype.up = function(num) {
   this.temperature = this.temperature + num;
+}
+
+Thermostat.prototype.down = function(num) {
+  this.temperature = this.temperature - num;
 }
