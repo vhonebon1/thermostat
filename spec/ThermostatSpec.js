bf0de91d499ba1f23d4382c55ab9895ab45cf3f7
Verity <vhonebon@gmail.com>
2017-07-05 14:38:48 +0100

    Added set max temp and turn power saving off functions

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,9 +10,6 @@ describe("Thermostat", function() {
     expect(thermostat.MINIMUM).toEqual(10);
   });
 
-
-
-
   it ('initializes with a temperature of 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
   });
@@ -28,6 +25,26 @@ describe("Thermostat", function() {
     it ('decreases the temperature', function() {
       thermostat.down(5)
       expect(thermostat.temperature).toEqual(15)
+    });
+  });
+
+  describe('setMaxTemp', function() {
+    it("sets the maximum temperature if power saving mode is on", function() {
+      spyOn(thermostat, "powerSavingMode").and.returnValue(true)
+      expect(thermostat.maximum).toEqual(25)
+    });
+
+    it("sets the maximum temperature if power saving mode is off", function() {
+      thermostat.turnPowerSavingOff()
+      thermostat.setMaxTemp()
+      expect(thermostat.maximum).toEqual(32)
+    });
+  });
+
+  describe('turnPowerSavingOff', function() {
+    it("turns the power saving mode off", function() {
+      thermostat.turnPowerSavingOff()
+      expect(thermostat.powerSavingMode).toEqual(false)
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,8 @@
 function Thermostat() {
   this.temperature = 20;
   this.MINIMUM = 10;
+  this.maximum = 25;
+  this.powerSavingMode = true;
 }
 
 Thermostat.prototype.up = function(num) {
@@ -9,4 +11,14 @@ Thermostat.prototype.up = function(num) {
 
 Thermostat.prototype.down = function(num) {
   this.temperature = this.temperature - num;
+}
+
+Thermostat.prototype.setMaxTemp = function() {
+  if (this.powerSavingMode == false)  {
+  this.maximum = 32
+  }
+}
+
+Thermostat.prototype.turnPowerSavingOff = function() {
+  this.powerSavingMode = false
 }
