25d7c132d2789402ac4b3635e54f1676e546349d
terminalobject <doctorboredom@hotmail.com>
2017-07-07 17:01:37 +0100

    mostly got ajax requests working

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require 'sinatra/base'
 require 'json'
-
+require 'sinatra/cookies'
 class Thermostat < Sinatra::Base
   enable :sessions
-
+  helpers Sinatra::Cookies
+  set :public_folder, 'public'
   get "/" do
     headers 'Access-Control-Allow-Origin' => '*'
-    data = session[:data]
-    content_type :json
-    { data: data }.to_json
+    erb :display
+    # data = session[:data]
+    # content_type :json
+    # { data: data }.to_json
     # temperature = session[:temperature]
     # city = session[:city]
     # city_temperature = session[:city_temperature]
@@ -16,7 +18,10 @@ class Thermostat < Sinatra::Base
 
   get "/temperature" do
     headers 'Access-Control-Allow-Origin' => '*'
+    'Hello'
+    p headers
     p session
+    p cookies.keys
     content_type :json
     { data: session[:data] }.to_json
   #   content_type :json
